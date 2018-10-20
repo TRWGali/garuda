0a312072028d1af01504bb7e888ac73d34526c7d
Taylor White <34773851+TRWGali@users.noreply.github.com>
2018-10-20 12:05:35 -0400

    remove home component

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import Home from '../components/Home';
-
+//remove home component
 export class HomePage extends React.Component<RouteComponentProps<any>, void> {
   render() {
     return (
